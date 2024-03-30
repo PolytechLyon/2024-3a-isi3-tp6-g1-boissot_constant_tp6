d61a03312e29db90633150505e0266eb9d646d91
Aurélien BOISSOT <aurelien.boissot@gmail.com>
2024-03-30 11:43:52 +0100

    code exercice 5

--- src/main/java/fr/polytech/sim/log/NamedLogger.java
+++ src/main/java/fr/polytech/sim/log/NamedLogger.java
@@ -16,4 +16,12 @@ public abstract class NamedLogger implements Logger {
         this.name = name;
     }
 
+    public void log(String format, Object... args) {
+        String entry = String.format(format, args);
+        String message = String.format("%s\t%s\n", this.name, entry);
+        this.output(message);
+    }
+
+    public abstract void output(String message);
+
 }
